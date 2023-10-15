64bed5dbc58b792654f7137094529a33e1323c52
M4h45amu7x <lovelovethekiss123@gmail.com>
2023-10-15 23:11:23 +0700

    Updated response types

--- src/types/Response.ts
+++ src/types/Response.ts
@@ -24,6 +24,7 @@ interface SuccessResponse {
     data: {
         transRef: string
         date: string
+        countryCode: string
         amount: {
             amount: number
             local: {
