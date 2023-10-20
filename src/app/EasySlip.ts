c45d89f065610031f8c9ee4cf39fbb5bf48c0302
M4h45amu7x <lovelovethekiss123@gmail.com>
2023-10-20 13:03:11 +0700

    Updated verifyByImage feature

--- src/app/EasySlip.ts
+++ src/app/EasySlip.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosError } from 'axios'
+import FormData from 'form-data'
+import fs from 'fs'
 
 import EasySlipVerifyError from '../exceptions/EasySlipVerifyError'
 import type { ErrorResponse, SuccessResponse } from '../types/Response'
@@ -21,18 +23,50 @@ class EasySlip {
     }
 
     /**
-     * Verifies the payload using the EasySlip API.
+     * Verifies the slip by payload using the EasySlip API.
      *
      * @param {string} payload - The data payload to be verified.
      * @returns {Promise<SuccessResponse>} - A promise that resolves to the verification result.
      * @throws {EasySlipVerifyError} - If the verification request fails.
      */
-    public async verify(payload: string): Promise<SuccessResponse> {
+    public async verifyByPayload(payload: string): Promise<SuccessResponse> {
         try {
             const { data } = await axios.get<SuccessResponse>(this.endpoint, {
                 params: {
                     payload,
                 },
+                headers: {
+                    Authorization: `Bearer ${this.key}`,
+                },
+            })
+
+            return data
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                if (error.response)
+                    throw new EasySlipVerifyError(
+                        `Request failed with status code: ${error.response.data.status}`,
+                        error.response.data as ErrorResponse,
+                    )
+            }
+
+            throw error
+        }
+    }
+
+    /**
+     * Verifies the slip by image using the EasySlip API.
+     *
+     * @param {string} imagePath - The image to be verified.
+     * @returns {Promise<SuccessResponse>} - A promise that resolves to the verification result.
+     * @throws {EasySlipVerifyError} - If the verification request fails.
+     */
+    public async verifyByImage(imagePath: string): Promise<SuccessResponse> {
+        try {
+            const formData = new FormData()
+            formData.append('file', fs.createReadStream(imagePath))
+
+            const { data } = await axios.post<SuccessResponse>(this.endpoint, formData, {
                 headers: {
                     Authorization: `Bearer ${this.key}`,
                 },
--- test/verify.spec.ts
+++ test/verify.spec.ts
@@ -2,10 +2,21 @@ import { expect } from 'chai'
 
 import { EasySlip } from '../src'
 
-describe('Verify', () => {
+describe('Verify By Payload', () => {
     it('Should success', async () => {
         const slip = new EasySlip('TEST')
-        const data = await slip.verify('TEST')
+        const data = await slip.verifyByPayload('TEST')
+
+        expect(data).to.contains({
+            status: 200,
+        })
+    })
+})
+
+describe('Verify By Image', () => {
+    it('Should success', async () => {
+        const slip = new EasySlip('TEST')
+        const data = await slip.verifyByImage('test/images/qrcode.jpeg')
 
         expect(data).to.contains({
             status: 200,
